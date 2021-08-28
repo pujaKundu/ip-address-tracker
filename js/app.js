d91f086cf8d936f190c45c6defa46be9f080c151
Puja Kundu <pujakundu797@gmail.com>
2021-08-29 01:26:47 +0600

    map update

--- js/app.js
+++ js/app.js
@@ -35,8 +35,8 @@ function buildMap(lat, lng, ip) {
     document.getElementById('mapUI').innerHTML =
         "<div id='map' style='width: 100%; height: 500px;'></div>";
     var map = L.map('map').setView([lat, lng], 10);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     L.marker([lat, lng]).addTo(map)
         .bindPopup(ip)
