31f235e4f52ede767f450e5c01995874aba547a0
Puja Kundu <pujakundu797@gmail.com>
2021-08-29 01:32:34 +0600

    map

--- js/app.js
+++ js/app.js
@@ -38,6 +38,7 @@ function buildMap(lat, lng, ip) {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
     L.marker([lat, lng]).addTo(map)
         .bindPopup(ip)
         .openPopup();
