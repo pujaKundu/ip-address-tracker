8b92dbde3d03500ce72afdfc86e0ce2f838426ca
Puja Kundu <pujakundu797@gmail.com>
2021-08-29 01:37:25 +0600

    commit

--- js/app.js
+++ js/app.js
@@ -34,11 +34,10 @@ const displayData = ip => {
 function buildMap(lat, lng, ip) {
     document.getElementById('mapUI').innerHTML =
         "<div id='map' style='width: 100%; height: 500px;'></div>";
-    var map = L.map('map').setView([lat, lng], 10);
+    var map = L.map('map').setView([lat, lng], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-
     L.marker([lat, lng]).addTo(map)
         .bindPopup(ip)
         .openPopup();
